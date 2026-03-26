967a59a818b79e12baf85c2b8d602016c7028d63
manideep-nelluri <166906777+manideep-nelluri@users.noreply.github.com>
2026-03-26 11:35:43 +0530

    first commit

---
+++ scipt.js
@@ -0,0 +1,7 @@
+ const hamburgur=document.querySelector(".hamburgur");
+            const menu_bar=document.querySelector(".navbar-menu .menu-bar")
+
+            hamburgur.addEventListener("click",()=>{
+            menu_bar.classList.toggle("show")
+            })
+            menu_bar.classList.toggle("show")
